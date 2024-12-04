03963f00104e0f73762e0d1c3471864289ebc316
eyalahyani <eyalahyani.info@gmail.com>
2024-12-05 00:18:19 +0300

    First commit

---
+++ src/main/java/org/LahyaniEya/App_Buro/AppBuroApplication.java
@@ -0,0 +1,14 @@
+package org.LahyaniEya.App_Buro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBuroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/LahyaniEya/App_Buro/AppBuroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LahyaniEya.App_Buro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBuroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
